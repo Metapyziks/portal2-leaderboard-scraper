18a94f0206704d3b0e4b18afe533fd6481b682d6
James King <james.king@facepunchstudios.com>
2018-01-07 14:53:55 +0000

    Can now generate histogram data

--- LeaderboardScraper/Program.cs
+++ LeaderboardScraper/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Runtime.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LeaderboardScraper
 {
@@ -77,23 +81,73 @@ namespace LeaderboardScraper
         [KnownType(typeof(LeaderboardInfo))]
         public class LeaderboardsResponse : Collection<LeaderboardInfo>
         {
-            [DataMember(Name="appID", Order = 0)]
+            [DataMember(Name="appID", Order=0)]
             public int AppId { get; set; }
 
-            [DataMember(Name="appFriendlyName", Order = 1)]
+            [DataMember(Name="appFriendlyName", Order=1)]
             public string AppFriendlyName { get; set; }
 
-            [DataMember(Name="leaderboardCount", Order = 2)]
+            [DataMember(Name="leaderboardCount", Order=2)]
             public int LeaderboardCount { get; set; }
+        }
+        
+        [DataContract(Name="entry", Namespace="")]
+        public class EntryInfo
+        {
+            [DataMember(Name="steamid", Order=0)]
+            public ulong SteamId { get; set; }
+
+            [DataMember(Name="score", Order=1)]
+            public int Score { get; set; }
+
+            [DataMember(Name="rank", Order=2)]
+            public int Rank { get; set; }
+
+            [DataMember(Name="ugcid", Order=3)]
+            public ulong UgcId { get; set; }
+        }
+
+        [DataContract(Name="response", Namespace="")]
+        [KnownType(typeof(EntryInfo))]
+        public class LeaderboardEntriesResponse
+        {
+            [DataMember(Name="appID", Order=0)]
+            public int AppId { get; set; }
+
+            [DataMember(Name="appFriendlyName", Order=1)]
+            public string AppFriendlyName { get; set; }
+            
+            [DataMember(Name="leaderboardID", Order=2)]
+            public int LeaderboardId { get; set; }
+            
+            [DataMember(Name="totalLeaderboardEntries", Order=3)]
+            public int TotalLeaderboardEntries { get; set; }
+            
+            [DataMember(Name="entryStart", Order=4)]
+            public int EntryStart { get; set; }
+            
+            [DataMember(Name="entryEnd", Order=5)]
+            public int EntryEnd { get; set; }
+            
+            [DataMember(Name="nextRequestURL", Order=6)]
+            public string NextRequestUrl { get; set; }
+            
+            [DataMember(Name="resultCount", Order=7)]
+            public int ResultCount { get; set; }
+
+            [DataMember(Name="entries", Order=8)]
+            public Collection<EntryInfo> Entries { get; } = new Collection<EntryInfo>();
         }
 
         private readonly DataContractSerializer _leaderboardsSerializer;
+        private readonly DataContractSerializer _leaderEntriesSerializer;
         private HttpClient _client;
 
         public SteamStats()
         {
             _client = new HttpClient();
             _leaderboardsSerializer = new DataContractSerializer(typeof(LeaderboardsResponse));
+            _leaderEntriesSerializer = new DataContractSerializer(typeof(LeaderboardEntriesResponse));
         }
         
         public LeaderboardsResponse GetLeaderboards(string game)
@@ -110,6 +164,21 @@ namespace LeaderboardScraper
                 return (LeaderboardsResponse) _leaderboardsSerializer.ReadObject(stream);
             }
         }
+        
+        public LeaderboardEntriesResponse GetLeaderboardEntries(string url)
+        {
+            var task = GetLeaderboardEntriesAsync(url);
+            task.Wait();
+            return task.Result;
+        }
+
+        public async Task<LeaderboardEntriesResponse> GetLeaderboardEntriesAsync(string url)
+        {
+            using (var stream = await _client.GetStreamAsync(url))
+            {
+                return (LeaderboardEntriesResponse) _leaderEntriesSerializer.ReadObject(stream);
+            }
+        }
 
         public void Dispose()
         {
@@ -118,8 +187,51 @@ namespace LeaderboardScraper
         }
     }
 
+    class HistogramData
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("intervalSize")]
+        public int IntervalSize { get; set; }
+
+        [JsonProperty("minScore")]
+        public int MinScore { get; set; }
+
+        [JsonProperty("maxScore")]
+        public int MaxScore { get; set; }
+
+        [JsonProperty("totalEntries")]
+        public int TotalLeaderboardEntries { get; set; }
+        
+        [JsonProperty("requestedEntries")]
+        public int RequestedLeaderboardEntries { get; set; }
+
+        [JsonProperty("nextRequestUrl")]
+        public string NextRequestUrl { get; set; }
+
+        [JsonProperty("values")]
+        public int[] Values { get; private set; }
+
+        public HistogramData() { }
+
+        public HistogramData(SteamStats.LeaderboardInfo leaderboard)
+        {
+            Name = leaderboard.Name;
+            IntervalSize = leaderboard.DisplayType == SteamStats.DisplayType.Score ? 1 : 50;
+            MinScore = 0;
+            MaxScore = leaderboard.DisplayType == SteamStats.DisplayType.Score ? 100 : 5 * 60 * 100;
+            NextRequestUrl = leaderboard.Url;
+            RequestedLeaderboardEntries = 0;
+            TotalLeaderboardEntries = leaderboard.Entries;
+            Values = new int[(MaxScore - MinScore + IntervalSize - 1) / IntervalSize];
+        }
+    }
+
     class Program
     {
+        static readonly DateTime _sStartTime = DateTime.Now;
+
         static int ReadIndex(int min, int max)
         {
             int index;
@@ -135,77 +247,137 @@ namespace LeaderboardScraper
         {
             const string game = "Portal2";
 
-            SteamStats.LeaderboardsResponse leaderboards;
-
             using (var stats = new SteamStats())
             {
-                leaderboards = stats.GetLeaderboards(game);
+                var leaderboards = stats.GetLeaderboards(game);
                 Console.WriteLine($"Found {leaderboards.Count} leaderboards.");
-            }
 
-            var path = new List<string>();
+                var path = new List<string>();
 
-            SteamStats.LeaderboardInfo leaderboard;
+                SteamStats.LeaderboardInfo leaderboard;
 
 browsing:
-            {
-                Console.WriteLine();
-                Console.WriteLine($"Browsing /{string.Join("/", path)}:");
-
-                var entries = leaderboards
-                    .Where(x => x.IsInPath(path) && x.NameParts.Length > path.Count)
-                    .GroupBy(x => x.NameParts[path.Count])
-                    .OrderBy(x => x.Count() == 1 ? 1 : 0)
-                    .ToArray();
-
-                if (path.Count > 0)
                 {
-                    Console.WriteLine($"  {0}:\t../");
+                    Console.WriteLine();
+                    Console.WriteLine($"Browsing /{string.Join("/", path)}:");
+
+                    var entries = leaderboards
+                        .Where(x => x.IsInPath(path) && x.NameParts.Length > path.Count)
+                        .GroupBy(x => x.NameParts[path.Count])
+                        .OrderBy(x => x.Count() == 1 ? 1 : 0)
+                        .ToArray();
+
+                    if (path.Count > 0)
+                    {
+                        Console.WriteLine($"  {0}:\t../");
+                    }
+
+                    var i = 1;
+                    foreach (var entry in entries)
+                    {
+                        Console.WriteLine($"  {i++}:\t{(entry.Count() == 1 ? entry.First().DisplayName : entry.Key + "/")}");
+                    }
+
+                    i = ReadIndex(path.Count > 0 ? 0 : 1, entries.Length);
+
+                    if (i == 0) path.RemoveAt(path.Count - 1);
+                    else if(entries[i - 1].Count() == 1)
+                    {
+                        leaderboard = entries[i - 1].First();
+                        goto selected;
+                    }
+                    else path.Add(entries[i - 1].Key);
+
+                    goto browsing;
                 }
-
-                var i = 1;
-                foreach (var entry in entries)
-                {
-                    Console.WriteLine($"  {i++}:\t{(entry.Count() == 1 ? entry.First().DisplayName : entry.Key + "/")}");
-                }
-
-                i = ReadIndex(path.Count > 0 ? 0 : 1, entries.Length);
-
-                if (i == 0) path.RemoveAt(path.Count - 1);
-                else if(entries[i - 1].Count() == 1)
-                {
-                    leaderboard = entries[i - 1].First();
-                    goto selected;
-                }
-                else path.Add(entries[i - 1].Key);
-
-                goto browsing;
-            }
 
 selected:
-            {
-                Console.WriteLine();
-                Console.WriteLine($"Selected {leaderboard.DisplayName} ({leaderboard.Entries:N} entries).");
-                Console.WriteLine($"  {0}:\tBack to browse");
-                Console.WriteLine($"  {1}:\tGenerate histogram");
-
-                switch (ReadIndex(0, 1))
                 {
-                    case 0:
-                        goto browsing;
-                    case 1:
-                        GenerateHistogram(leaderboard);
-                        goto selected;
+                    Console.WriteLine();
+                    Console.WriteLine($"Selected {leaderboard.DisplayName} ({leaderboard.Entries:N} entries).");
+                    Console.WriteLine($"  {0}:\tBack to browse");
+                    Console.WriteLine($"  {1}:\tGenerate histogram data");
+
+                    switch (ReadIndex(0, 1))
+                    {
+                        case 0:
+                            goto browsing;
+                        case 1:
+                            var cancellationSource = new CancellationTokenSource();
+                            var task = GenerateHistogram(stats, leaderboard, cancellationSource.Token);
+
+                            Console.ReadKey(false);
+
+                            if (!task.IsCompleted)
+                            {
+                                cancellationSource.Cancel();
+                            }
+
+                            goto selected;
+                    }
                 }
             }
         }
 
-        static void GenerateHistogram(SteamStats.LeaderboardInfo leaderboard)
+        static async Task GenerateHistogram(SteamStats stats, SteamStats.LeaderboardInfo leaderboard, CancellationToken cancel)
         {
             Console.WriteLine();
             Console.WriteLine($"Generating histogram for {leaderboard.DisplayName}.");
+            Console.WriteLine("Press any key to cancel...");
+            Console.WriteLine();
 
-            Console.ReadKey(true);
+            var asmDir = Path.GetDirectoryName(typeof(Program).GetTypeInfo().Assembly.Location);
+            var leaderboardDir = Path.Combine(asmDir, "leaderboards", $"{leaderboard.Name}.json");
+            var destFilePath = Path.Combine(leaderboardDir, $"{_sStartTime:yyyy-dd-M}.json");
+
+            if (!Directory.Exists(leaderboardDir)) Directory.CreateDirectory(leaderboardDir);
+
+            var data = File.Exists(destFilePath)
+                ? JsonConvert.DeserializeObject<HistogramData>(File.ReadAllText(destFilePath))
+                : new HistogramData(leaderboard);
+
+            while (!cancel.IsCancellationRequested && data.RequestedLeaderboardEntries < data.TotalLeaderboardEntries)
+            {
+                SteamStats.LeaderboardEntriesResponse entries;
+                
+                try
+                {
+                    entries = await stats.GetLeaderboardEntriesAsync(data.NextRequestUrl);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                }
+
+                Console.WriteLine($"Fetched {entries.EntryEnd} of {entries.TotalLeaderboardEntries} entries...");
+
+                foreach (var entry in entries.Entries)
+                {
+                    if (entry.Score < data.MinScore) continue;
+                    if (entry.Score >= data.MaxScore)
+                    {
+                        Console.WriteLine("Reached max score.");
+                        data.RequestedLeaderboardEntries = data.TotalLeaderboardEntries;
+                        break;
+                    }
+
+                    var index = (entry.Score - data.MinScore) / data.IntervalSize;
+
+                    data.Values[index]++;
+                }
+
+                if (entries.EntryEnd > data.RequestedLeaderboardEntries)
+                {
+                    data.RequestedLeaderboardEntries = entries.EntryEnd;
+                    data.NextRequestUrl = entries.NextRequestUrl;
+                }
+
+                File.WriteAllText(destFilePath, JsonConvert.SerializeObject(data));
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
         }
     }
 }
